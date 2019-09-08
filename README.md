21239953f203367dcd2b929d0efde7f9fd474f78
Cemetary Doll <rakw1995@gmail.com>
2019-09-08 19:47:07 -0400

    completes assignment and edits read me

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # E02a-Operators
+This code samples various different operators:
+* Arithmetic (add, subtract, multiply, divide, intergers/ floor division,exponents, modulo, order of operations)
+* Checking data types (int, float, str, bool, tuple, list, dict)
+* Converting numbers
+* Concatenating strings
+* Slicing strings and substrings
+* Reversing strings
+* Finding items within a given list (and proving true or false)
+* Returning a random item from a given list
+* Returning a random number within a given range
+* Reversing a given list
+* Shuffling the order of a list
+* Making a new list in sequence within range
 
+The code then prints the operations.
--- main.py
+++ main.py
@@ -64,7 +64,7 @@ def orderOperations(a1,a2,a3):
     i.e., changes normal order or operations 
     '''
     toReturn = 0
-    toReturn = (a1 + a2) * a3
+    toReturn = (a1 + a2)* a3
     return toReturn
 
 def whichType(t):
@@ -72,7 +72,7 @@ def whichType(t):
     Returns the python data type of t
     '''
     toReturn = 0
-    toReturn = 0
+    toReturn = type(t)
     return toReturn
 
 def convertInt(i):
@@ -80,7 +80,7 @@ def convertInt(i):
     Converts i to an integer
     '''
     toReturn = 0
-    # add your code here
+    toReturn = int(i)
     return toReturn
 
 def convertFloat(f):
@@ -88,31 +88,31 @@ def convertFloat(f):
     Converts f to a float
     '''
     toReturn = 0.0
-    # add your code here
+    toReturn = float(f)
     return toReturn
 
 def convertStr(s):
     '''
     Converts s to a string
     '''
-    toReturn = ''
-    # add your code here
+    toReturn = 0
+    toReturn = str(s)
     return toReturn
 
 def concat(s1,s2):
     '''
     Concatenates (string) s2 to the end of s1
     '''
-    toReturn = ''
-    # add your code here
+    toReturn = 0
+    toReturn = str.join(s1,s2)
     return toReturn
 
 def whichChar(s,a):
     '''
     Returns the character at position a of string s
     '''
-    toReturn = ''
-    # add your code here
+    toReturn = 0
+    toReturn = (s[a::])
     return toReturn
 
 def substr(s,a1,a2):
@@ -120,7 +120,7 @@ def substr(s,a1,a2):
     Returns a substring of s (from position a1 to a2)
     '''
     toReturn = ''
-    # add your code here
+    toReturn =  (s[a1:a2:])
     return toReturn
 
 def reverseStr(s):
@@ -128,15 +128,15 @@ def reverseStr(s):
     Returns a string which is a reversed version of s
     '''
     toReturn = ''
-    # add your code here
+    toReturn = ''.join(reversed(s))
     return toReturn
 
 def isIn(l,e):
     '''
     Returns true if element e is in list l
     '''
-    toReturn = ''
-    # add your code here
+    toReturn = 0
+    toReturn = (e) in l 
     return toReturn
 
 def randomElement(l):
@@ -144,7 +144,7 @@ def randomElement(l):
     Returns a random element of list l
     '''
     toReturn = ''
-    # add your code here
+    toReturn = random.choice(l)
     return toReturn
 
 def randomNumber():
@@ -152,7 +152,7 @@ def randomNumber():
     Returns a random number between 1000 and 9999
     '''
     toReturn = 0
-    # add your code here
+    toReturn = random.randint (1000,9999)
     return toReturn
 
 def reverseList(l):
@@ -160,7 +160,7 @@ def reverseList(l):
     Returns a list which is a mirror of l (each element is present in reverse order from l)
     '''
     toReturn = []
-    # add your code here
+    toReturn = list(reversed(l))
     return toReturn
 
 def shuffleList(l):
@@ -168,7 +168,7 @@ def shuffleList(l):
     Returns a list which has all the elements of l in a random order
     '''
     toReturn = []
-    # add your code here
+    toReturn = random.shuffle(l),l
     return toReturn
 
 def listUntil(a):
@@ -176,7 +176,7 @@ def listUntil(a):
     Returns a list contains numbers from 0 to a
     '''
     toReturn = []
-    # add your code here
+    toReturn = list (range(0,a))
     return toReturn
 
 
